3aa9338660848273d14d17fb40435e95893cb19c
Dan Reardon <dreardon1021@gmail.com>
2020-04-02 10:33:34 -0600

    Write tests for Form

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -22,19 +22,47 @@ describe('Form', () => {
     expect(rezBtnEL).toBeInTheDocument()
   })
 
+  it('should be able to update input values', () => {
+    const {  getByPlaceholderText } = render(<Form />)
+
+    const nameInputEl = getByPlaceholderText("Name")
+    const dateInputEl = getByPlaceholderText("Date")
+    const timeInputEl = getByPlaceholderText("Time")
+    const numberInputEl = getByPlaceholderText("Number Of Guests")
+
+    fireEvent.change(getByPlaceholderText("Name"), { target: {value: 'Dan'} })
+    fireEvent.change(getByPlaceholderText("Date"), { target: {value: '2020-04-22'} })
+    fireEvent.change(getByPlaceholderText("Time"), { target: {value: '6:00'} })
+    fireEvent.change(getByPlaceholderText("Number Of Guests"), { target: {value: '2'} })
+
+    expect(nameInputEl.value).toBe('Dan')
+    expect(dateInputEl.value).toBe('2020-04-22')
+    expect(timeInputEl.value).toBe('6:00')
+    expect(numberInputEl.value).toBe('2')
+  })
+
   it('should be able to add a new reservation', () => {
     const mockAddReservation = jest.fn()
 
-    const { getByText, getByPlaceholderText, getByTestId } = render(
+    const { getByText, getByPlaceholderText } = render(
       <Form
         addReservation={mockAddReservation}
       />)
 
-    fireEvent.change(getByPlaceholderText("Name"), { target: {value: 'Dan'} })  
-    fireEvent.change(getByPlaceholderText("Date"), { target: {value: '04/22'} })
+    fireEvent.change(getByPlaceholderText("Name"), { target: {value: 'Dan'} })
+    fireEvent.change(getByPlaceholderText("Date"), { target: {value: '2020-04-22'} })
     fireEvent.change(getByPlaceholderText("Time"), { target: {value: '6:00'} })
     fireEvent.change(getByPlaceholderText("Number Of Guests"), { target: {value: '2'} })
 
-    fireEvent.click(getByText(""))
+    fireEvent.click(getByText("Make Reservation"))
+
+    expect(mockAddReservation).toHaveBeenCalledWith(
+      {
+        name: 'Dan',
+        date: '2020-04-22',
+        time: '6:00',
+        number: '2'
+      }
+    )
   })
 })
